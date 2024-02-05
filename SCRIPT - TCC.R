091db5b926c860543c4f576f2b03c7dadd27f7fb
ALEF1001RS <146146970+ALEF1001RS@users.noreply.github.com>
2024-02-04 21:29:55 -0300

    Add files via upload

--- SCRIPT - TCC.R
+++ SCRIPT - TCC.R
@@ -1,11 +1,11 @@
 
 ## MBA USP - ESALQ
-## TCC - Cria??o de um aplicativo para ajudar candidatos do ENEM (Sisu) de cursos de exatas na escolha da universidade
+## TCC - Criacao de um aplicativo para ajudar candidatos do ENEM (Sisu) de cursos de exatas na escolha da universidade
 
 
 # Alef Rodolfo dos Santos
 
-# Instala??o e carregamento dos pacotes utilizados
+# Instalacao e carregamento dos pacotes utilizados
 
 pacotes <- c("plotly", #plataforma gr?fica
              "tidyverse", #carregar outros pacotes do R
@@ -21,7 +21,9 @@ pacotes <- c("plotly", #plataforma gr?fica
              "readxl",# fun??o para leitura do Excel
              "shiny", #biblioteca Shiny do R para desenvolver app
               "dplyr", 
-             "data.table")
+             "data.table",
+             "ggmap")
+
 
 
 if(sum(as.numeric(!pacotes %in% installed.packages())) != 0){
@@ -43,7 +45,7 @@ load(file = ".RData")
 
 
 
-dados <- read_csv('notas_do_ENEM_1.csv')
+dados <- read_xlsx('notas_do_ENEM_1.xlsx')
 
 
 # Visualiza??o da base de dados
@@ -53,13 +55,9 @@ dados %>%
                 full_width = FALSE,
                 font_size = 20)
 
+
+
 colnames(dados)
-
-library(ggplot2)
-
-
-
-
 view(dados)
 
 
@@ -68,13 +66,20 @@ view(dados)
 
 #Data Manipulation
 
+library(ggmap)
+library(ggplot2)
+library(sp)
+
+
 
 
 categoria <- 'Cota Rc EP 1,5sm'
 
 curso <- c('Eng Ambiental','Eng Química')
 
+
 região <- c('Sudeste','Nordeste')
+
 
 
 estado<-c('Paraná','Pernambuco')
@@ -91,23 +96,16 @@ theme_set(theme_bw())
 
 
 
-
-unid<-dados_select%>%
-  group_by(Unidade)
-  ggplot(dados_select) +
-  geom_line(aes(x=Curso, y = Nota))
-  
-  dtable<-dados
-  
-
 ##=======================================##
   
   dados$Categoria
+  
   length(unique(dados$Categoria))
   
   
   
   dados$Curso
+  
   length(unique(dados$Curso))
   
   dados$Estado
@@ -117,78 +115,102 @@ unid<-dados_select%>%
   length(unique(dados$Região))
   
  
-view(dados)
+
+  view(dados)
   
 
-dtable<-dados
-##---------- Esquema de aglomera??o hier?rquico---------------------------------
+
+
+  dtable<-dados
+##---------- Esquema de aglomeração hierárquico---------------------------------
 
 # Matriz de dissimilaridades
-matriz_D <- dados %>%
-  select(Nota, Ensino, Pesquisa, Mercado, Inova??o , Internacionaliza??o) %>% 
+matriz_D <- dtable %>% 
+  select(Nota, Ensino, Pesquisa, Mercado, Inovação, Internacionalização) %>% 
   dist(method = "euclidean")
 
-## Em 'dist.binary', method = 2 indica similaridade por emparelhamento simples
+# Method: parametrização da distância a ser utilizada
 
+## "euclidean": distância euclidiana
+## "euclidiana quadrática": elevar ao quadrado matriz_D (matriz_D^2)
+## "maximum": distância de Chebychev;
+## "manhattan": distância de Manhattan (ou distância absoluta ou bloco);
+## "canberra": distância de Canberra;
+## "minkowski": distância de Minkowski
 
 # Visualizando a matriz de dissimilaridades
 data.matrix(matriz_D) %>% 
   kable() %>%
   kable_styling(bootstrap_options = "striped", 
                 full_width = FALSE, 
-                font_size = )
+                font_size = 20)
+
+# Elaboração da clusterização hierárquica
+cluster_hier <- agnes(x = matriz_D, method = "complete")
 
 
-# Elabora??o da clusteriza??o hier?rquica
-cluster_hier <- agnes(x = matriz_D, method = "average")
+# O input é a matriz de distâncias obtida anteriormente
 
-# Outras op??es de encadeamento:
+# Method é o tipo de encadeamento:
 
 ## "complete": encadeamento completo (furthest neighbor ou complete linkage)
 ## "single": encadeamento único (nearest neighbor ou single linkage)
 ## "average": encadeamento médio (between groups ou average linkage)
 
-# Defini??o do esquema hier?rquico de aglomera??o
+# Definição do esquema hierárquico de aglomeração
 
-# As dist?ncias para as combina??es em cada est?gio
+# As distâncias para as combinações em cada estágio
 coeficientes <- sort(cluster_hier$height, decreasing = FALSE) 
 coeficientes
 
-# Tabela com o esquema de aglomera??o. Interpreta??o do output:
 
-## As linhas são os estágios de aglomera??o
+
+# Tabela com o esquema de aglomeração. Interpretação do output:
+
+## As linhas são os estágios de aglomeração
 ## Nas colunas Cluster1 e Cluster2, observa-se como ocorreu a junção
 ## Quando for número negativo, indica observação isolada
 ## Quando for número positivo, indica cluster formado anteriormente (estágio)
 ## Coeficientes: as distâncias para as combinações em cada estágio
 
 esquema <- as.data.frame(cbind(cluster_hier$merge, coeficientes))
-
 names(esquema) <- c("Cluster1", "Cluster2", "Coeficientes")
-
 esquema
 
 
-
-# Visualiza??o do esquema hier?rquico aglomerativo
+# Visualização do esquema hierárquico de aglomeração
 esquema %>%
   kable(row.names = T) %>%
   kable_styling(bootstrap_options = "striped", 
                 full_width = FALSE, 
                 font_size = 20)
 
+# Construção do dendrograma
+dev.off()
+fviz_dend(x = cluster_hier)
+
+DT::datatable(matriz_D)
 
 
+# Dendrograma com visualização dos clusters (definição de 3 clusters)
+fviz_dend(x = cluster_hier,
+         k = 3,
+        k_colors = c("deeppink4", "darkviolet", "deeppink"),
+       color_labels_by_k = F,
+      rect = T,
+     rect_fill = T,
+    lwd = 1,
+   ggtheme = theme_bw())
 
+# Criando variável categórica para indicação do cluster no banco de dados
+## O argumento 'k' indica a quantidade de clusters
+dtable$cluster_H <- factor(cutree(tree = cluster_hier, k = 3))
 
 # Visualização da base de dados com a alocação das observações nos clusters
-dados %>%
-  select(Universidade, cluster_H) %>%
-  arrange(Universidade) %>% 
+ dtable %>%
   kable() %>%
-  kable_styling(bootstrap_options = "striped", 
-                full_width = FALSE,
-                font_size = 20)
+ kable_styling(bootstrap_options = "striped", 
+              full_width = FALSE,
+             font_size = 20)
 
-# FIM!
 
